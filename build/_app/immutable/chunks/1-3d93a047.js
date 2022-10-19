8a8d8be6ab726c9ecc525269e54689e7f564c233
Dominik Kormann <dominikormann@outlook.com>
2022-10-19 22:22:33 +0200

    build

---
+++ build/_app/immutable/chunks/1-3d93a047.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-56787780.js";export{t as component};
---
+++ build/_app/immutable/chunks/4-d3e12970.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/leaderboard/_page.svelte-75f32ed6.js";export{t as component};
